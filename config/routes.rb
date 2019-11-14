8d93969f024244688abf1ad1096ecad3fc6fdea4
NMP300 <t.NMP300@gmail.com>
2019-11-14 11:02:30 +0900

    Initial devise settings.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {sessions: "users/sessions"}
   scope "(:locale)", locale: /en|ja/ do
     resources :books
   end
+
+  root to: "books#index"
 end
