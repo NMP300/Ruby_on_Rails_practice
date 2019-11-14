3395c70e40395f8abfce016f059b645600678884
NMP300 <t.NMP300@gmail.com>
2019-11-14 15:46:25 +0900

    Added user pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/show'
   scope "(:locale)", locale: /en|ja/ do
     resources :books
     devise_for :users, controllers: {
         registration: "users/registrations",
         sessions: "users/sessions"
     }
-    resources :users, only: [:show]
+    resources :users, only: [:index, :show]
   end
   root to: "books#index"
 end
