b2c48a149007a988c84ca62430d92dd9f6908627
NMP300 <t.NMP300@gmail.com>
2019-11-15 09:11:28 +0900

    Added books, login, logout and edit profile links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
   scope "(:locale)", locale: /en|ja/ do
     resources :books
     devise_for :users, controllers: {
-        registration: "users/registrations",
-        sessions: "users/sessions"
+        registrations: "users/registrations",
+        sessions: "users/sessions",
     }
     resources :users, only: [:index, :show]
   end
