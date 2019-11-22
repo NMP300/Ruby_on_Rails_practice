65475ba0d88de8d1bb63e42bf08db0026bf51e47
NMP300 <t.NMP300@gmail.com>
2019-11-22 15:30:40 +0900

    Added function of uploading file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :address, :profile, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :profile, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:icon, :name, :postal_code, :address, :profile, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:icon, :name, :postal_code, :address, :profile, :password, :password_confirmation])
   end
 
   private
