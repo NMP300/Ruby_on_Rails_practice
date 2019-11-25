81f59c6ece6c3d56ca2a8278f79d265a0500ea61
NMP300 <t.NMP300@gmail.com>
2019-11-25 16:36:00 +0900

    Fixed by rubocop.
    # Conflicts:
    #	config/initializers/devise.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: {
-      omniauth_callbacks: 'users/omniauth_callbacks'
+      omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   scope "(:locale)", locale: /en|ja/ do
