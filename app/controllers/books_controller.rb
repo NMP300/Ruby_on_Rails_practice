d5e77ad402ad0dd7722c839dc1c4c9c743ac4855
NMP300 <t.NMP300@gmail.com>
2019-11-28 16:41:23 +0900

    Added timeline and my page.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -41,6 +41,7 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
       params.require(:book).permit(:title, :memo, :author, :picture, :user_id)
     end
