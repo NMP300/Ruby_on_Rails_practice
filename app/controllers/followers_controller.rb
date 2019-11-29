1e63f777f8fb0bd14c3e442d8b3b3926e35a8955
NMP300 <t.NMP300@gmail.com>
2019-11-29 17:44:35 +0900

    Internationalized view related follow.

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,6 +1,5 @@
 class FollowersController < ApplicationController
   def index
-    @title = "Followers"
     @user = User.find(params[:id])
     @users = @user.followers.page params[:page]
   end
--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -1,6 +1,5 @@
 class FollowingController < ApplicationController
   def index
-    @title = "Following"
     @user = User.find(params[:id])
     @users = @user.following.page params[:page]
   end
