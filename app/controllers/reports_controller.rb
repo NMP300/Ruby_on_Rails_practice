720203401dfa7bee83bd919ae2ed2110bc51b189
NMP300 <t.NMP300@gmail.com>
2019-12-01 13:43:37 +0900

    Added initial function of report by scaffold.

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,40 @@
+class ReportsController < ApplicationController
+  before_action :set_report, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @reports = Report.all
+  end
+
+  def show
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def edit
+  end
+
+  def create
+    @report = Report.new(report_params)
+  end
+
+  def update
+  end
+
+  def destroy
+    @report.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def report_params
+    params.require(:report).permit(:title, :text)
+  end
+end
