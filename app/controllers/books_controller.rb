c4c3278f7d4de3623cc47a99af73dbbca939f2cd
NMP300 <t.NMP300@gmail.com>
2019-12-01 11:17:34 +0900

    Fixed by rubocop.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  # GET /books
-  # GET /books.json
   def index
     @books = current_user.books.page params[:page]
   end
