6ae2ee159da512394c1be2f8eaeca4d754ec57fc
NMP300 <t.NMP300@gmail.com>
2019-12-02 14:44:45 +0900

    Changed to display both the book list and report list on the user profile screen.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,14 @@ class UsersController < ApplicationController
   end
 
   def show
+    # 改善点：冗長な記述
     @user = User.find(params[:id])
-    @books = Book.where(user_id: @user.id).page params[:page]
+    @posts = []
+    @books = Book.where(user_id: @user.id)
+    @reports = Report.where(user_id: @user.id)
+    @posts << @books
+    @posts << @reports
+    @posts.flatten!.sort_by! { |post| post[:created_at] }
   end
 end
 
