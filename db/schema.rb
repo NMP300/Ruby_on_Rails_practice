b85b0110fbe6151acbf67552f2073e5aaa0cd559
NMP300 <t.NMP300@gmail.com>
2019-12-11 16:37:34 +0900

    Added the model test related user model.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_12_02_062452) do
-  create_table "active_storage_attachments", force: :cascade do |t|
+
+create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.integer "record_id", null: false
---
+++ test/model/user_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  setup do
+    @user1 = users(:Taro)
+    @user2 = users(:Hanako)
+  end
+
+  test "登録済みのユーザーをomniauth認証情報から発見することができる" do
+    auth_params = OmniAuth::AuthHash.new(uid: "12345", provider: "github")
+    auth_user = User.from_omniauth(auth_params)
+    assert auth_user == @user1
+  end
+
+  test "user1がuser2をフォローすることができる" do
+    @user1.follow(@user2)
+    assert [@user2] == @user1.following
+  end
+
+  test "user1がuser2をフォローしている時、user2のフォローを解除することができる" do
+    @user1.follow(@user2)
+    @user1.unfollow(@user2)
+    assert_no_match @user2, @user1.following
+  end
+
+  test "user1がuser2をフォローしている時、user1のfollowingの中にuser2がいることを確認できる" do
+    @user1.follow(@user2)
+    assert @user2 = @user1.following?(@user2)
+  end
+end
